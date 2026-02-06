9c0288b3035fc6b7089eb7e00ae0209c1fce9d5d
SoyMoises <fabianibanez2001@gmail.com>
2026-02-06 19:51:51 -0300

    Refactor script.js to improve button functionality

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ const getRandomNumber = (num) => {return Math.floor(Math.random() * (num + 1));}
       yesButton.style.fontSize = `${fontSize}px`;
   
       //no button text
-      const messages = ["No","Are you sure?","Babyy please?","Don't do this to me :(","Say yes or else...",];
+      const messages = ["No","Estas Segura?","Enserio????","No me hagas esto bb :(","Di que si o...",];
   
       if (noClickCount === 4) {
         const newButton = document.createElement("button");
@@ -154,4 +154,5 @@ const getRandomNumber = (num) => {return Math.floor(Math.random() * (num + 1));}
   
     move();
   }
+
   
